89f123258d751ec8b18e2e6f1040686e86c6ec98
brenosekiro <b88660501@gmail.com>
2020-12-12 21:36:10 -0300

    final

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: RandomWords(),
+      theme: ThemeData(
+        primaryColor: Colors.purple
+      ),
+    );
+  }
+}
